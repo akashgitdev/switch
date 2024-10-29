ebb4ca2c1127b125170e523feecda17a5faedf41
akashgitdev <nagaakashduddupudi1712@email.com>
2024-10-29 16:57:06 +0530

    commit

---
+++ practice.js
@@ -0,0 +1,27 @@
+// var arr=[3,5,10,15,23,22,25,35,56];
+// var arr1=[];
+// for (i in arr){
+//     if(arr[i]%5==0)
+//       console.log(arr[i])  
+    // arr[arr1.length]=arr[i]
+        // arr1.push(arr[i])
+// }
+
+
+
+// var arr=["html","css","javascript","angular","react"];
+// var arr1=[","]
+// // for(i in arr){
+// //     if(i%2==0)
+// //         arr1.push(arr[i])
+// // }
+// console.log(arr1.toUpperCase())
+
+
+
+// var str=["hai","haI"];
+// for (i in str){
+//    if(str[i]!=str[i].toLowerCase())
+      
+//     console.log(str[i])
+// }
